ae964928579e6e0ae9143781714e2d3edecf8ae6
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-23 22:20:53 +0000

    edited app.py to add status code

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,7 +19,7 @@ def tearDown(self):
 @app.errorhandler(404)
 def page_not_found(self):
     """ handles 404 error """
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
