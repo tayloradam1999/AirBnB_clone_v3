5f4fef18a3690df24befe6d5e67a4bd2f4fecfee
Adam Taylor <tayloradam1999@gmail.com>
2021-06-23 04:01:34 +0000

    Put Blueprint before wildcar import all

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Host of our our Flask app"""
-
-
+"""Host of our Flask app"""
 from flask import Flask, Blueprint, render_template
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -16,7 +15,5 @@ def tearDown(self):
     storage.close()
 
 if __name__ == "__main__":
-    app.config.update(
-        SERVER_NAME="HBNB_API_HOST:HBNB_API_PORT"
-    )
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=getenv("HBNB_API_PORT", "5000"), threaded=True)
