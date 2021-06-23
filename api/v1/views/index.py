e99e10c147dfa04ff9b49a6f80bf6a9c857db259
Adam Taylor <tayloradam1999@gmail.com>
2021-06-23 03:21:50 +0000

    Added MORE Documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 
 @app_views.route("/status", strict_slashes=False)
 def return_jsonify():
+    """Returns JSON status=OK"""
     return jsonify(
         status=OK,
     )
