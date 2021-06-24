9a081581df78d3fbc22244dde2a7cf447c1bffa8
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-24 01:00:44 +0000

    add states.py for checker and good check point

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-
+import json
 
 @app_views.route("/states/<string:state_id>", methods=['GET'],
                  strict_slashes=False)
@@ -17,15 +17,29 @@ def show_with_id(state_id):
     if data is None:
         abort(404)
     return jsonify(data.to_dict())
-
+        
 
 @app_views.route("/states", methods=['GET'], strict_slashes=False)
 def show_all():
     """ by default, shows all states """
 
     states = storage.all(State).values()
-    if request.method == 'GET':
-        new_list = []
-        for state in states:
-            new_list.append(state.to_dict())
-        return jsonify(new_list)
+    new_list = []
+    for state in states:
+        new_list.append(state.to_dict())
+    return jsonify(new_list)
+
+
+@app_views.route("/states/<string:state_id>", methods=['DELETE'],
+                 strict_slashes=False)
+def delete_with_id(state_id):
+    """ deletes the class associated with given id """
+
+    data = storage.get(State, state_id)
+    if data is None:
+        abort(404)
+    storage.delete(data)
+    storage.save()
+    return jsonify({}), 200
+
+
