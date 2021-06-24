485c37b28616a5afee81ab2880c8cfd1f43275d4
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-24 00:26:18 +0000

    add id functionality

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 """ New view for Stat objects that handles default API actions """
 
-from flask import Flask, Blueprint, jsonify, request, url_for
+from flask import Flask, Blueprint, jsonify, request, url_for, abort
 from models.base_model import BaseModel
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
+@app_views.route("/states/<string:state_id>", methods=['GET'],
+                 strict_slashes=False)
+def show_with_id(state_id):
+    """ shows specific class with given id """
+
+    data = storage.get(State, state_id)
+    if data is None:
+        abort(404)
+    return jsonify(data.to_dict())
+
+
 @app_views.route("/states", methods=['GET'], strict_slashes=False)
 def show_all():
     """ by default, shows all states """
-    
+
     states = storage.all(State).values()
-    
     if request.method == 'GET':
         new_list = []
         for state in states:
